8c96ceea904c3188ae3ec0b44ea9c58aadf78227
Marcel de Wit <marcel.dewit@teamrockstars.nl>
2021-06-11 17:17:22 +0200

    WIP articles

---
+++ src/platform/Controllers/ArticleBlockController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class ArticleBlockController : Controller
+    {
+        public ActionResult ArticleBlock()
+        {
+            return View("~/Views/Articles/ArticleBlock.cshtml");
+        }
+    }
+}
